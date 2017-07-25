34e6fd3e5f52bb451639f38f9e6f6ed2f7aee33f
Gollum <4113481@qq.com>
2017-07-25 11:57:21 +0800

    Created basic xocdeproj and parse input

--- Package.swift
+++ Package.swift
@@ -3,5 +3,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Locals"
+    name: "Locals",
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine.git", "3.0.0-pre1"),
+        .Package(url: "https://github.com/onevcat/Rainbow", "2.0.1")
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,56 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p",
+                                 longFlag: "project",
+                                 helpMessage: "Path to the output file.")
+
+let excludedPaths = MultiStringOption(shortFlag: "e",
+                      longFlag: "exclude",
+                      helpMessage: "Prints a help message.")
+
+let help = BoolOption(shortFlag: "h",
+                      longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+cli.addOptions(projectOption, help)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+        break
+    case .optionFlag:
+        str = s.green.underline
+        break
+    case .optionHelp:
+        str = s.lightBlue
+        break
+    default:
+        str = s
+        break
+    }
+    return cli.defaultFormat(s: str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+print("File path is \(projectOption.value!)".red.bold)
+
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+let project = projectOption.value ?? "."
+
+let fileExtensions = "strings"
